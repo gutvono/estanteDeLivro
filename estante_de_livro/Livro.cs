81864f67c2555fdec5244d3afa8d6406c9e8ba29
Gustavo <guvono4@gmail.com>
2024-04-30 14:36:11 -0300

    update: novas funcoes
    - imprimir livro especifico
    - editar livro

--- estante_de_livro/Livro.cs
+++ estante_de_livro/Livro.cs
@@ -1,31 +1,31 @@
-﻿public class Livro
+﻿internal class Livro
 {
     private string titulo;
     private string autor;
     private DateOnly dataLancamento;
     private string editora;
     private string edicao;
-    private int iSBN;
+    private string iSBN;
     private int qtdPaginas;
-
-    public Livro() { }
 
     public string Titulo { get => titulo; set => titulo = value; }
     public string Autor { get => autor; set => autor = value; }
     public DateOnly DataLancamento { get => dataLancamento; set => dataLancamento = value; }
     public string Editora { get => editora; set => editora = value; }
     public string Edicao { get => edicao; set => edicao = value; }
-    public int ISBN { get => iSBN; set => iSBN = value; }
+    public string ISBN { get => iSBN; set => iSBN = value; }
     public int QtdPaginas { get => qtdPaginas; set => qtdPaginas = value; }
 
     public void ImprimirLivro()
     {
-        Console.WriteLine($"Titulo: {this.Titulo}");
-        Console.WriteLine($"Autor: {this.Autor}");
-        Console.WriteLine($"Data de lancamento: {this.DataLancamento}");
-        Console.WriteLine($"Editora: {this.Editora}");
-        Console.WriteLine($"Edicao: {this.Edicao}");
-        Console.WriteLine($"ISBN: {this.ISBN}");
-        Console.WriteLine($"Quantidade de paginas: {this.QtdPaginas}");
+        Console.WriteLine(
+            $"1 - Titulo: {this.Titulo}\n" +
+            $"2 - Autor: {this.Autor}\n" +
+            $"3 - Data de lancamento: {this.DataLancamento}\n" +
+            $"4 - Editora: {this.Editora}\n" +
+            $"5 - Edicao: {this.Edicao}\n" +
+            $"6 - ISBN: {this.ISBN}\n" +
+            $"7 - Quantidade de paginas: {this.QtdPaginas}"
+            );
     }
 }
--- estante_de_livro/Program.cs
+++ estante_de_livro/Program.cs
@@ -1,15 +1,18 @@
 ﻿int QtdLivros = 0, opcao = 0;
-Livro[] Livros = new Livro[1];
+Livro[] Estante = new Livro[1];
 
 void Menu()
 {
     do
     {
-        Console.WriteLine("Insira o numero relacionado ao que você precisa:");
-        Console.WriteLine("1 - Criar estante de livros;");
-        Console.WriteLine("2 - Adicionar livros na estante;");
-        Console.WriteLine("3 - Mostrar livros da estante;");
-        Console.WriteLine("4 - Sair do sistema.");
+        Console.WriteLine(
+            "Insira o numero relacionado ao que você precisa:\n" +
+            "1 - Criar estante de livros;\n" +
+            "2 - Adicionar livros na estante;\n" +
+            "3 - Mostrar livros da estante;\n" +
+            "4 - Imprimir um livro específico;\n" +
+            "5 - Sair do sistema."
+            );
         opcao = int.Parse(Console.ReadLine());
 
         switch (opcao)
@@ -23,17 +26,29 @@ void Menu()
             case 3:
                 MostrarLivros();
                 break;
+            case 4:
+                Console.Write("Informe o índice do livro");
+                BuscarLivro(int.Parse(Console.ReadLine()) - 1);
+                break;
+            case 5:
+                break;
             default:
+                Console.WriteLine("ERRO: Insira um número válido.");
                 break;
         }
-    } while (opcao != 4);
+    } while (opcao != 5);
 }
 
 void CriarEstante()
 {
-    Console.Write("Informe a quantidade de livros que deseja guardar: ");
-    QtdLivros = int.Parse(Console.ReadLine());
-    Livros = new Livro[QtdLivros];
+    do
+    {
+        if (QtdLivros > 10) Console.WriteLine("Nossa estante só terá espaço para até 10 livros. Favor inserir um valor menor que 10!");
+        
+        Console.Write("Informe a quantidade de livros que deseja guardar: ");
+        QtdLivros = int.Parse(Console.ReadLine());
+        Estante = new Livro[QtdLivros];
+    } while (QtdLivros > 10);
 }
 
 Livro CriarLivro(int indice)
@@ -57,7 +72,7 @@ Livro CriarLivro(int indice)
     livro.Edicao = Console.ReadLine();
 
     Console.Write("\nInforme o ISBN: ");
-    livro.ISBN = int.Parse(Console.ReadLine());
+    livro.ISBN = Console.ReadLine();
 
     Console.Write("\nInforme a quantidade de paginas: ");
     livro.QtdPaginas = int.Parse(Console.ReadLine());
@@ -69,7 +84,7 @@ void AdicionarLivros()
 {
     for (int l = 0; l < QtdLivros; l++)
     {
-        Livros[l] = CriarLivro(l);
+        Estante[l] = CriarLivro(l);
     }
 }
 
@@ -78,10 +93,61 @@ void MostrarLivros()
     for (int l = 0; l < QtdLivros; l++)
     {
         Console.WriteLine("\n--------------------------------------------------\n");
-        Livros[l].ImprimirLivro();
+        Estante[l].ImprimirLivro();
         Console.WriteLine("\n--------------------------------------------------\n");
     }
 }
 
-//-----PROGRAMA-----
+void BuscarLivro(int i)
+{
+    Estante[i].ImprimirLivro();
+    Console.WriteLine
+    (
+        "Deseja editar este livro?\n" +
+        "1 - SIM\n" +
+        "2 - NÃO"
+    );
+    int editar = int.Parse(Console.ReadLine());
+
+    if (editar == 1) Estante[i] = EditarLivro(Estante[i]);
+}
+
+Livro EditarLivro(Livro livro)
+{
+    Livro l = livro;
+    Console.WriteLine("\nQual índice deseja editar?");
+    int i = int.Parse(Console.ReadLine()); 
+    
+    switch (i)
+    {
+        case 1:
+            l.Titulo = Console.ReadLine();
+            break;
+        case 2:
+            l.Autor = Console.ReadLine();
+            break;
+        case 3:
+            l.DataLancamento = DateOnly.Parse(Console.ReadLine());
+            break;
+        case 4:
+            l.Editora = Console.ReadLine();
+            break;
+        case 5:
+            l.Edicao = Console.ReadLine();
+            break;
+        case 6:
+            l.ISBN = Console.ReadLine();
+            break;
+        case 7:
+            l.QtdPaginas = int.Parse(Console.ReadLine());
+            break;
+        default:
+            Console.WriteLine("ERRO: Insira um índice válido.");
+            break;
+    }
+
+    return l;
+}
+
+//-----PROGRAMA-----//
 Menu();
